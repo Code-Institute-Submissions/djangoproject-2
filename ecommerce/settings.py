0710458aeb63ce3040ff8fa758a6a09d93147cd2
oyvindwagner <oyvind.wagner@gmail.com>
2020-09-23 10:35:26 +0200

    Updated gitignore + key

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4dp%8!c!9c@olh@(vgf!!a(p8nw@!=s***497=2p!0m=4))rh!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
