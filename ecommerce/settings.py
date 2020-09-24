4a304f0b70761a7f89df29fc7b50f2883ca6cc89
oyvindwagner <oyvind.wagner@gmail.com>
2020-09-24 13:16:42 +0200

    Added procfile + shipping model

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import django_heroku
 import os
 from os import path
 from pathlib import Path
@@ -129,3 +130,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
